d2377928e55dda52f5a36d5600608494b74e5a6d
HasnainAlikehar <hasnainalimpb@gmail.com>
2024-02-24 15:45:17 +0500

    1 to 5 javascript file upload

---
+++ app.js
@@ -0,0 +1,237 @@
+// Chapter : 1   { My first greeting message Showing in (Alert-box) }
+
+
+// var greeting = "Hello users...! i hope that you i'll be like this J.S code";
+// alert(greeting);
+
+
+
+// Chapter : 1     { Question No: 1 }
+
+// var passwrodError = "Error! Please enter a valid password.";
+// alert(passwrodError);
+
+
+
+// Chapter : 1     { Question No: 2 }
+
+// var line1 = "Welcome to JS Land...";
+// var line2 = "Happy Coding!";
+// var message = line1+"\n"+line2;
+// alert(message);
+
+
+
+// Chapter : 1     { Question No: 3 }
+
+// var welcome = "Welcome to JS Land...";
+// alert(welcome);
+// var happy = "Happy Coding!";
+// var prevent = "Prevent this page from creating additional dialogs.";
+// alert(happy+"\n"+prevent);
+
+
+
+// Chapter : 1   { Question No 4: } { Generate the message through browser's developer console : }
+
+// var hello = ("Hello... I can run Js through my web browser's console")  [ copy ] this code and paste in web browser console then [ Enter ]
+// alert(hello)                                                            [ copy ] this code and paste in web browser console then [ Enter ] then you can see alert Msg
+
+
+
+// chapter 2 Question 1 { Declare a var called username } 
+
+// var username;
+
+
+
+
+// chapter 2 Question 2 { Declare a var called myNAme & assign in string }
+
+// var myName = "Hasnain Ali";
+
+
+
+
+// chapter 2 Question 3 
+
+// var message = " Hello World";
+// alert(message);
+
+
+
+
+// chapter 2 Question 4 { Students bio data in var & showing in the alert boxes}
+
+// var fullName = "Jhone Doe";
+// var age = "15 years old";
+// var faculty = "Certified mobile Application Development";
+// alert(fullName);
+// alert(age);
+// alert(faculty);
+
+
+
+
+// chapter 2 Question 5
+
+// var line1 = "PIZZA";
+// var line2 = "PIZZ";
+// var line3 = "PIZ";
+// var line4 = "PI";
+// var line5 = "P";
+// alert(line1+"\n"+line2+"\n"+line3+"\n"+line4+"\n"+line5);
+
+
+
+
+// chapter 2 Question 6 { Showing Email Address in Alert box }
+
+// var email = "hasnainalimpb@gmail.com";
+// alert("My email address is " + email );
+
+
+
+
+// chapter 2 Question 7
+
+// var book = "A smarter \n way to learn JavaScript";
+// alert("i'm trying to learn from the Book " + book);
+
+
+
+// chapter 2 Question 8
+
+// var content = "Yah! I can write HTML content through Javascript";
+// document.write(content);
+
+// document.write("<p>Yah! I can write HTML content through JavaSript</p>");
+
+
+
+// chapter 2 Question 9
+
+// var design = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”" ;
+// alert(design) ;
+
+
+
+// chapter 3 Question 1
+
+// var age = 15 ;
+// alert("i am " + age + " years old ") ;
+
+
+
+// chapter 3 Question 2 { how many times a visitor has visited a web page show in alert-box}
+
+// var content = "You have visited this site " ;
+// var times = "14 times" ;
+// alert( content + times ) ;
+
+
+
+// chapter 3 Question 3 { show content on web page by using code in javaScript }
+
+// var birthyear = "My birth year is " ;
+// var year = 1997 ;
+// var data = "Data type of my ddeclared variable is number" ;
+// document.write( birthyear + year + "<br />" + data ) ;
+
+
+
+
+// chapter 3 Question 4  { online clothing store  }
+
+
+// var fullName = prompt("Enter Your Fullname") ;
+// var itmes = prompt("How many T-Shirts you want") ;
+// var confirm = confirm("Are You Confirmed ?") ;
+// document.write( fullName + " ordered " + itmes + " T-shirt(s) on XYZ Clothing Store" ) ;
+
+
+
+
+
+
+// chapter 4 Question 1 
+
+// var head = "<h1>Rules for naming JS variables</h1>" ;
+// var dash = "_" ;
+// var dollar = "$" ;
+
+// document.write(head)
+// document.write("Variables names can only contain , numbers , "+ dollar + " and " + dash + " . For example : " + dollar + "my" + dash + "1stVariable" + "<br />");
+// document.write("Variable must begin with a letter," + dollar + " or " + dash + ". For example : " + dollar + "name , "+ dash +"name or name " + "<br />");
+// document.write("Variables names are case sensitive" + "<br />");
+// document.write("Variables names should not be JS keywords");
+
+ 
+
+
+
+// chapter 5 satrt     Question 1 
+
+// var num1 = +prompt("Enter your number");
+// var num2 = +prompt("Enter your number");
+// var total = num1 + num2;
+// document.write("sum of " + num1 + " and " + num2 + " is "+ total) ;
+
+// chapter 5 Question 2 
+
+// var num1 = +prompt("Enter your number") ;
+// var num2 = +prompt("Enter your number") ;
+// var total = num1 - num2 ;
+// document.write("sum of " + num1 + " and " + num2 + " is "+ total) ;
+
+// chapter 5 Question 2 multiplication
+
+// var num1 = +prompt("Enter your number") ;
+// var num2 = +prompt("Enter your number") ;
+// var total = num1 * num2 ;
+// document.write("sum of " + num1 + " and " + num2 + " is "+ total) ;
+
+// chapter 5 Question 2 division
+
+// var num1 = +prompt("Enter your number");
+// var num2 = +prompt("Enter your number");
+// var total = num1 / num2 ;
+// document.write("sum of " + num1 + " and " + num2 + " is "+ total);
+
+// chapter 5 Question 2 division
+
+// var num1 = +prompt("Enter your number");
+// var num2 = +prompt("Enter your number");
+// var total = num1 % num2 ;
+// document.write("sum of " + num1 + " and " + num2 + " is "+ total);
+
+
+
+// chapter 5 Question 4
+
+// var ticket = +prompt("Ticket Quantity");
+// var price = ticket * 600 ;
+// document.write("The cost to buy " + ticket + " tickets to a movie is " + price + "PKR");
+
+
+
+
+// chapter 5 Question 5 { write a Table }
+
+//  var ask =  +prompt("Enter The Table Number ");
+
+//  var num1 = ask + "x 1 =" + ask * 1 ;
+//  var num2 = ask + "x 2 =" + ask * 2 ;
+//  var num3 = ask + "x 3 =" + ask * 3 ;
+//  var num4 = ask + "x 4 =" + ask * 4 ; 
+//  var num5 = ask + "x 5 =" + ask * 5 ;
+//  var num6 = ask + "x 6 =" + ask * 6 ;
+//  var num7 = ask + "x 7 =" + ask * 7 ;
+//  var num8 = ask + "x 8 =" + ask * 8 ;
+//  var num9 = ask + "x 9 =" + ask * 9 ;
+//  var num10 = ask + "x 10 =" + ask * 10 ;
+
+//  document.write("Table of "+ ask + "<br />"+ num1 +"<br />"+ num2 +"<br />"+ num3+"<br />"+ num4+"<br />"+ num5+"<br />"+ num6 
+//  +"<br />"+ num7+"<br />"+ num8+"<br />"+ num9+"<br />"+ num10 )
+ 
+
